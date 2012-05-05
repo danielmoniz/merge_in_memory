7d385edf10bb3d05b7b37da4e7950341a39f4802
Daniel Moniz <daniel.moniz@gmail.com>
2012-05-04 22:42:24 -0400

    Make installable with setup.py. Improve README.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,3 @@
 import merge_in_memory
+
+__version__ = '0.1'
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MergeInMemory',
+    version='0.1',
+    description='In-Memory Versioning Tool',
+    author='Daniel Moniz',
+    author_email='daniel.moniz@gmail.com',
+    url='https://github.com/danielmoniz/merge_in_memory',
+    py_modules=['merge_in_memory'],
+)
